1a113c3959d3e477a196f5f30e1c58e9481527bc
haveyouwantto <haveyouwantto@gmail.com>
2020-08-20 22:12:55 +0800

    Create zhcpp.h

---
+++ zhcpp.h
@@ -0,0 +1,73 @@
+#pragma once
+
+typedef int 整数;
+typedef float 单精度;
+typedef double 双精度;
+typedef char 字符;
+typedef bool 布尔;
+typedef long long 长整数;
+typedef void 无类型;
+typedef std::string 字符串;
+
+#define 加 +
+#define 减 -
+#define 乘 *
+#define 除 /
+#define 设为 =
+#define 等于 ==
+#define 加上 +=
+#define 减去 -=
+#define 乘上 *=
+#define 除去 /=
+
+#define 增加 ++
+#define 减少 --
+
+#define 小于 <
+#define 大于 >
+#define 不等于 !=
+#define 小于等于 <=
+#define 大于等于 >=
+
+#define 不是 !
+#define 或者 ||
+#define 以及 &&
+
+#define 则 ?
+#define 不然 :
+#define 的 .
+
+#define 如果 if
+#define 否则 else
+#define 执行 do
+#define 只要 while
+#define 对于 for
+#define 尝试执行 try
+#define 捕获 catch
+#define 检测 switch
+#define 如果为 case
+#define 退出 break
+#define 继续 continue
+#define 返回 return
+
+#define 换行符 "\n"
+
+#define 类 class
+#define 新建 new
+#define 删除 delete
+#define 常量 const
+#define 无符号 unsigned
+#define 公开成员 public
+#define 受保护成员 protected
+#define 私有成员 private
+
+#define 无穷大 INFINITY
+#define 无穷小 -INFINITY
+#define 非实数 NAN
+
+#define 主程序 main
+#define 格式化输出 printf
+
+#define 标准输出 std::cout
+#define 标准输入 std::cin
+#define 标准错误输出 std::cerr
